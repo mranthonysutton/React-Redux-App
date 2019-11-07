1212dc80ae8ab9c2f288b552d1039e5352391a01
Anthony Sutton <anthony.sutton95@gmail.com>
2019-11-07 14:14:25 -0800

    Axios call built out. Need to build in the reducer

--- openbrewerydb/src/actions/index.js
+++ openbrewerydb/src/actions/index.js
@@ -1,5 +1,16 @@
+import axios from "axios";
+
 export const FETCH_START = "FETCH_START";
 export const FETCH_SUCCESS = "FETCH_SUCCESS";
 export const FETCH_FAILURE = "FETCH_FAILURE";
 
 // Need to set our axios call here
+
+export const fetchBreweryData = () => dispatch => {
+  dispatch({ type: FETCH_START });
+
+  axios
+    .get("https://api.openbrewerydb.org/breweries")
+    .then(response => console.log(response))
+    .catch(error => console.log(error));
+};
